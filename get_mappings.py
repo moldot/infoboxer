91c53af653a6563a3567b8cdefd65952d564139e
Varot Premtoon <molev.mt@gmail.com>
2015-03-28 14:52:32 -0400

    Able to retrieve and cache infobox pages. Edit infobox_parser to replace forward slash with hyphen

--- get_mappings.py
+++ get_mappings.py
@@ -1,30 +1,35 @@
 import urllib2
+import re
+from wikiscout import infobox_parser
 
-MAPPINGS_URL = 'http://mappings.dbpedia.org/index.php/Mapping_en'
-HTML_STORE_PATH = 'dbpedia/'
+MAPPINGS_URLS = [
+    'http://mappings.dbpedia.org/index.php?title=Special:AllPages&namespace=204&from=1930s-UK-film-stub&to=Infobox_darts_player',
+    'http://mappings.dbpedia.org/index.php?title=Special:AllPages&namespace=204&from=Infobox_diocese&to=US-child-writer-stub',
+    'http://mappings.dbpedia.org/index.php?title=Special:AllPages&namespace=204&from=US-company-stub&to=Year_dab'
+]
 
+URL_PREFIX = 'http://mappings.dbpedia.org'
+HTML_CACHE_PATH_PREFIX = 'dbpedia/'
 
-def get_page_and_store(url, store_path):
+def get_page_and_store(url, cache_path=None):
     """
     Fetch a html page from url and store in store_path
     """
 
     page = urllib2.urlopen(url).read()
 
-    if store_path is not None:
-        open(store_path, 'r').write(page)
+    if cache_path is not None:
+        open(cache_path, 'w').write(page)
 
     return page
 
 
-def get_infoboxes_from_mapping_page(mapping_page):
+def get_infobox_urls(mapping_page):
     """
-    Return (infobox, url) pairs, given the HTML DBpedia mapping page.
-
-    infobox is in 'Infobox <name>' format, as appeared in DBpedia Mapping page.
-    url directs to the mapping page for each infobox
+    Return list of urls of infobox pages
     """
-    pass
+    pattern = re.compile('/index\.php/Mapping_en:Infobox_[-\w\./]+')
+    return pattern.findall(mapping_page)
 
 
 def get_class_from_infobox_page(infobox_page):
@@ -35,15 +40,32 @@ def get_class_from_infobox_page(infobox_page):
     """
     pass
 
-
-def get_infobox_class_pairs():
+def get_infobox_class_pairs(from_cache=False):
     """
     Return pairs of (infobox, class)
 
-    infobox format is as returned by get_infoboxes_from_mapping_page.
+    infobox format is lower case with hyphen (e.g. 'afl-player-2')
     class format is as returbed by get_class_from_infobox_page.
     """
-    pass
+    infobox_urls = []
+    for mapping_url in MAPPINGS_URLS:
+        mapping_page = get_page_and_store(mapping_url)
+        infobox_urls += get_infobox_urls(mapping_page)
+
+    for i, infobox_url in enumerate(infobox_urls):
+        full_url = URL_PREFIX + infobox_url
+        infobox = infobox_parser.get_class(infobox_url.split(':')[1]).replace('wikipedia-', '')
+        cache_path = HTML_CACHE_PATH_PREFIX + 'infobox-' + infobox + '.html'
+
+        print '(%d/%d) %s' % (i+1, len(infobox_urls), infobox)
+        
+        if from_cache or i < 169:
+            infobox_page = open(cache_path, 'r').read()
+
+        else:
+            infobox_page = get_page_and_store(URL_PREFIX + infobox_url, cache_path)
 
 
+
+get_infobox_class_pairs()
 
